7db3491806cdf075a7df407d0048e3682bdafafd
prapawit201 <piyapat43@gmail.com>
2018-08-14 14:10:39 +0700

    MyAlpacaAll

---
+++ Alpaca/AlpacaLearn/hello-frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Mycomponent from './components/Mycomponent'
+import wipcamp from './ribbon-black-96x136.png'
+import wipcamp1 from './in-remembrance-of-his-majesty-king-bhumibol-adulyadej-5106535469416448.8-l.png'
+import './static/css/bootstrap.min.css';
+import './App.css'
+const Hello = () => (
+  <div class = "outer">
+    <div class = "C">
+    C  
+    </div>
+    <div class = "up">
+    up  
+    </div>
+  <div class = "code">
+    code
+  </div>
+    <div class ="inner">
+      
+
+
+    </div>
+  </div>
+)
+export default Hello
---
+++ Alpaca/AlpacaLearn/hello-frontend/src/components/Mycomponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Mycomponents = (props) => (
+    <div>
+        <h1>..............................</h1>
+        {props.message}
+        {props.name}
+</div>
+)
+export default Mycomponents
